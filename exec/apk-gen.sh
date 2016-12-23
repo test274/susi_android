2be5c74dee5db1e3ba943c0b1fdffeacb83f906d
Pranjal Paliwal <paliwal.pranjal83@gmail.com>
2016-12-24 00:26:51 +0530

    Updated Circle.yml for testing

--- exec/apk-gen.sh
+++ exec/apk-gen.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 
-git config --global user.name "the-dagger"
+git config --global user.name "betterclever"
 git config --global user.email "harshithdwivedi@gmail.com"
 
-git clone --quiet --branch=apk https://the-dagger:$GITHUB_API_KEY@github.com/fossasia/susi_android apk > /dev/null
+git clone --quiet --branch=apk https://betterclever:$GITHUB_KEY@github.com/betterclever/susi_android apk > /dev/null
 ls
 cp -r ${HOME}/${CIRCLE_PROJECT_REPONAME}/app/build/outputs/apk/app-debug.apk apk/susi-debug.apk
 cp -r ${HOME}/${CIRCLE_PROJECT_REPONAME}/app/build/outputs/apk/app-release-unsigned.apk apk/susi-release.apk
