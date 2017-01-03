a28876564bf47e9f09cab5c4de09617b33355d4e
betterclever <paliwal.pranjal83@gmail.com>
2017-01-03 23:58:26 +0530

    testing

--- exec/apk-gen.sh
+++ exec/apk-gen.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 set -e
 
-git config --global user.name "the-dagger"
-git config --global user.email "harshithdwivedi@gmail.com"
+if ! [ -z "$CIRCLE_PR_NUMBER" ]
+then
+	git config --global user.name "betterclever"
+	git config --global user.email "paliwal.pranjal83@gmail.com"
 
-git clone --quiet --branch=apk https://the-dagger:$GITHUB_API_KEY@github.com/fossasia/susi_android apk > /dev/null
-ls
-cp -r ${HOME}/${CIRCLE_PROJECT_REPONAME}/app/build/outputs/apk/app-debug.apk apk/susi-debug.apk
-cp -r ${HOME}/${CIRCLE_PROJECT_REPONAME}/app/build/outputs/apk/app-release-unsigned.apk apk/susi-release.apk
-cd apk
-git add .
-git commit -m "[Circle CI] Update Susi Apk"
-git push origin apk --quiet > /dev/null
+	git clone --quiet --branch=apk https://betterclever:$GITHUB_API_KEY@github.com/betterclever/susi_android apk > /dev/null
+	ls
+	cp -r ${HOME}/${CIRCLE_PROJECT_REPONAME}/app/build/outputs/apk/app-debug.apk apk/susi-debug.apk
+	cp -r ${HOME}/${CIRCLE_PROJECT_REPONAME}/app/build/outputs/apk/app-release-unsigned.apk apk/susi-release.apk
+	cd apk
+
+	git checkout --orphan workaround
+	git add -A
+
+	git commit -am "[Circle CI] Update Susi Apk"
+
+	git branch -D apk
+	git branch -m apk
+
+	git push origin apk --force --quiet > /dev/null
+fi
